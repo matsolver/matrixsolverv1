24c9048c25d75ddfd47c98a3566c1634db1ee22d
= <=>
2021-11-27 20:32:47 +0000

    setup

---
+++ matrixsolverv1/redpill.py
@@ -0,0 +1,10 @@
+def transpose2x2(my_matrix):
+    '''
+    Transposes your matrix.
+    Args:
+        my_matrix (i have no idea what datatype a list is): a 2x2 matrix to transpose
+    Returns:
+        my_matrix_transposed
+    
+    '''
+    print("this will be awesome one day")
