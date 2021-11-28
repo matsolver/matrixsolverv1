bbbd6409b14b39de1a82cf685a5139ef3e6e91de
luke12harris <luke12harris@gmail.com>
2021-11-28 11:25:16 +0000

    First Functions

--- matrixsolver/redpill.py
+++ matrixsolver/redpill.py
@@ -4,26 +4,41 @@ def transpose2x2 (my_mat):
     Transposes a 2x2 matrix.
 
     Args:
-        my_mat(list)
+        my_mat(nested list)
 
     Returns:
-        my_mat_t (list)
+        my_mat_t (nested list)
     '''
     my_mat_row1 = []
+    my_mat_row2 = []
 
-    my_mat_row1.append(bigboi[0][0])
-    my_mat_row1.append(bigboi[1][0])
+    my_mat_row1.append(my_mat[0][0])
+    my_mat_row1.append(my_mat[1][0])
         
-    my_mat_row2 =[]
-
-    my_mat_row2.append(bigboi[0][1])
-    my_mat_row2.append(bigboi[1][1])
+    my_mat_row2.append(my_mat[0][1])
+    my_mat_row2.append(my_mat[1][1])
 
     my_mat_t = [my_mat_row1, my_mat_row2]
-    print (my_mat_t)
+    return (my_mat_t)
+#####
+
+def det2x2 (my_mat):
+    '''
+    Finds the determinant of a 2x2 matrix.
+
+    Agrs:
+        my_mat(nested list)
+
+    Returns:
+        det_my_mat(nested list)
+    
+    '''
+
+    #ad - bc
+    return ((my_mat[0][0] * my_mat[1][1]) - (my_mat[0][1] * my_mat[1][0]))
+#####
+
 
 bigboi = [[1,2],[3,4]]
-transpose2x2(bigboi)
-
-
-
+print(transpose2x2(bigboi))
+print(det2x2(bigboi))
